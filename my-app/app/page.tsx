fea6996b77acb7e6ffa2a36bef37a6bbdb798e0e
Krishang <112581575+krishang-r@users.noreply.github.com>
2025-08-29 23:23:55 +0530

    frontend initial commit

---
+++ my-app/app/page.tsx
@@ -0,0 +1,15 @@
+import App from "@/components/Navbar"
+
+export default function Home(){
+  return(
+    <main className="h-2vh">
+      <App />
+      <div className="h-screen">
+        hello
+      </div>
+      <div className="h-screen">
+        hello
+      </div>
+    </main>
+  )
+}
---
+++ my-app/components/Navbar.tsx
@@ -0,0 +1,41 @@
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
+
+
+
+export default function App() {
+  return (
+    <Navbar className="bg-blue-200">
+      <NavbarBrand>
+        <img src="/Images/logo.png" alt="" className="max-h-18 mx-3 my-1"/>
+        <p className="font-bold text-inherit">ACME</p>
+      </NavbarBrand>
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Features
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link aria-current="page" href="#">
+            Customers
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Integrations
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#">Login</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="primary" href="#" variant="flat">
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
+}
